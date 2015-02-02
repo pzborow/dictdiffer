9e919c58b1ca34f8f309f388549bbbf95e97f889
Piotr Zborowski <pzborows@intercars.eu>
2015-02-02 15:49:35 +0100

    Changes version

--- dictdiffer/version.py
+++ dictdiffer/version.py
@@ -17,4 +17,4 @@ This file is imported by ``dictdiffer.__init__``, and parsed by
 # Do not change the format of this next line. Doing so risks breaking
 # setup.py and docs/conf.py
 
-__version__ = "0.4.0.dev20141208.pz"
+__version__ = "0.4888.pz"
